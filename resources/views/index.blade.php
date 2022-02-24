87e3eb6a15cfc735b03d46d2b02e5795acf0e366
EdRadical <ari.pa97@hotmail.it>
2022-02-24 13:07:28 +0100

    first index view

---
+++ resources/views/index.blade.php
@@ -0,0 +1,35 @@
+@extends('layouts.app')
+
+@section('content')
+    
+    <div class="container">
+
+        <div class="card text-center">
+            <div class="card-header">
+              <ul class="nav ">
+                <li class="nav-item">
+                  <a class="nav-link" href="#">Active</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#">Link</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#">Disabled</a>
+                </li>
+              </ul>
+            </div>
+
+            <div class="card-body">
+              <h5 class="card-title">Section</h5>
+            </div>
+
+            <div class="card-footer">
+                footer
+            </div>
+
+
+        </div>
+       
+    </div>
+
+@endsection  

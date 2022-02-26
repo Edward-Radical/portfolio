6322ae654e0a0a19eaa7d44f07aa6d743b832f9e
Arianna <ari.pa97@hotmail.it>
2022-02-26 16:48:58 +0100

    style

--- resources/views/link1.blade.php
+++ resources/views/link1.blade.php
@@ -2,9 +2,13 @@
 
 @section('content')
 
-{{-- {{dd($sound)}} --}}
+<header class="title d-flex align-items-center justify-content-center m-auto mt-4 mb-5">
+    <a href="javascript:history.back()"><img src="#" alt="" class="back_icon"></a>
+    <a href="index.html"><img src="#" alt="" class="home_icon"></a>
+    <h1>SOUND DESIGN PROJECTS</h1>
+</header>
 
-<div class="box d-flex flex-row justify-content-center flex-wrap m-0 p-0" style="border:2px solid red;">
+<div class="box d-flex flex-row justify-content-center flex-wrap mb-5">
     @foreach ($sound as $s )
 
         <x-box :title="$s->title" :immagine="$s->img" :link="$s->id" />
